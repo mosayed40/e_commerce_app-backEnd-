66f3b528d8a1640ee76717f67e90abfda95d8238
Mostafa Sayed <moustafa.2950@gmail.com>
2025-11-27 15:40:55 +0200

    Add Page Archive

---
+++ orders/archive.php
@@ -0,0 +1,9 @@
+<?php 
+
+include "../connect.php" ; 
+
+$userid = filterRequest("user_id") ; 
+
+getAllData('ordersview' , "orders_users_id = ? AND orders_status =  3" , [$userid]) ; 
+
+?>
--- orders/pending.php
+++ orders/pending.php
@@ -4,6 +4,6 @@ include "../connect.php" ;
 
 $userid = filterRequest("user_id") ; 
 
-getAllData('ordersview' , "orders_users_id = ? AND orders_status !=  4" , [$userid]) ; 
+getAllData('ordersview' , "orders_users_id = ? AND orders_status !=  3" , [$userid]) ; 
 
 ?>
